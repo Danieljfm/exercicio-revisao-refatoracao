8c2fb0b0b432a1cd0706df247316a4964199b33d
Danieljfm <66971040+Danieljfm@users.noreply.github.com>
2020-10-18 18:20:58 -0300

    Update Empregado.hpp
    Removi o atributo quotaMensalVendas e o passei para classe vendedor. Adicionei o atributo nome, pois todo empregado possui um nome. Além disso tornei os atributos privados e criei métodos para acessá-los. Também criei um atributo para armazenar o numero padrao de horas trabalhadas para ser mais facil uma possivel futura manutenção no software.

--- Empregado.hpp
+++ Empregado.hpp
@@ -7,22 +7,29 @@
 class Empregado {
 	
   public:
-    double salarioHora;  
-    double quotaMensalVendas;  
-
-
+	
+ double getsalarioHora() {return salarioHora;}
+    void setsalarioHora(double v){ salarioHora = v }
+    std::string getnome() { return nome;}
+    void setnome(std::string v){ nome = v }
+	
     double pagamentoMes(double horasTrabalhadas) {
  
       double t = horasTrabalhadas;
 	  
 	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
+      if (horasTrabalhadas > horaspadrao)
+        double x = horasTrabalhadas - horaspadrao ;
         t += x / 2;
       }
 	  return t * salarioHora;
     }
 	
 };
+private:
+ double salarioHora;   
+ std::string nome;
+int horaspadrao = 8;
+
 
 #endif
